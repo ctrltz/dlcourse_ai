Assignment 1: fixed cell output for KNN, added numeric gradient computation

assignments/assignment1/gradient_check.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def check_gradient(f, x, delta=1e-5, tol = 1e-4):
+def check_gradient(f, x, delta=1e-5, tol=1e-4):
     '''
     Checks the implementation of analytical gradient by comparing
     it to numerical gradient using two-point formula
@@ -26,15 +26,27 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     assert analytic_grad.shape == x.shape
     analytic_grad = analytic_grad.copy()
 
+    # Delta array for numeric gradient computation
+    x_delta = np.zeros(x.shape)
+
     # We will go through every dimension of x and compute numeric
     # derivative for it
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
         ix = it.multi_index
         analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
 
-        # TODO compute value of numeric gradient of f to idx
+        # Introduce delta at current index
+        x_delta[ix] = delta
+
+        # Compute numeric gradient
+        f_plus, _ = f(x + x_delta)
+        f_minus, _ = f(x - x_delta)
+        numeric_grad_at_ix = (f_plus - f_minus) / (2 * delta)
+
+        # Back to zero for further use
+        x_delta[ix] = 0
+
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
             print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
             return False